a4fccd7063a2e55a086439c1ff9afc79fb733e44
Sei Bellissima <63272339+Sei-Bellissima@users.noreply.github.com>
2020-12-13 22:58:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ function ErrorMessage(ErrorMsg, MsgToEdit, RequestMessage, WillEdit) {
         .setDescription("Looks like I've run into an error:\n\n`" + ErrorMsg + "`\n\nPlease ping my creator @Sei Bellissima to let her know about this!\n\n||If you are the one who summoned me, Sei, shame on you. <:rookgrin:736050803021840474> Now go and fix me!||")
         .setColor(0xa90000)
     if (WillEdit == true) {
-       MsgToEdit.edit(ErrorEmbed);
+        MsgToEdit.edit(ErrorEmbed);
     } else {
         RequestMessage.channel.send(ErrorEmbed);
     }
@@ -105,7 +105,7 @@ app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
 //LOG ON, then WAIT FOR MESSAGES
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("!fetchhelp"); 
+    client.user.setActivity("!fetchhelp");
 });
 
 client.on('message', async msg => {
@@ -119,10 +119,10 @@ client.on('message', async msg => {
     GlobalMessage = msg;
     //splitting message by first space
     var CardRequest, OriginalRequest, command, splitStr
-    if (body.indexOf(' ') >= 0){
-        command = body.substr(0,body.indexOf(' '));
-        OriginalRequest = body.substr(body.indexOf(' ')+1);
-        CardRequest = body.substr(body.indexOf(' ')+1);
+    if (body.indexOf(' ') >= 0) {
+        command = body.substr(0, body.indexOf(' '));
+        OriginalRequest = body.substr(body.indexOf(' ') + 1);
+        CardRequest = body.substr(body.indexOf(' ') + 1);
     } else command = body;
     if (command == "fetchhelp") {
         msg.channel.send(HelpEmbed)
@@ -130,15 +130,14 @@ client.on('message', async msg => {
         if (CardRequest.indexOf(" ") > -1) {
             splitStr = CardRequest.split(' ');
             for (var i = 0; i < splitStr.length; i++) {
-                if (splitStr[i] == " " || splitStr [i] == "") {
+                if (splitStr[i] == " " || splitStr[i] == "") {
                     let removed = splitStr.splice(i, 1);
                     i--;
                 };
             }
             CardRequest = splitStr.join(' ');
         };
-        IconToFetch = CardRequest.replace(/[- ]/g,"_");
-        IconToFetch = IconToFetch.replace(/[,.':!]/g, "");
+        IconToFetch = CardRequest.replace(/[- ]/g, "_").replace(/[,.':!]/g, "");
         var ImageLink = _.get(database, IconToFetch + '.icon');
         if (typeof ImageLink !== 'undefined') {
             const attachment = new MessageAttachment(ImageLink);
@@ -154,7 +153,7 @@ client.on('message', async msg => {
             splitStr = CardRequest.split(' ');
             for (var i = 0; i < splitStr.length; i++) {
                 if (splitStr[i] != "battle" && splitStr[i] != "negotiation" && splitStr[i] != "of") {
-                    if (splitStr[i] == " " || splitStr [i] == "") {
+                    if (splitStr[i] == " " || splitStr[i] == "") {
                         let removed = splitStr.splice(i, 1);
                         i--;
                     } else {
@@ -170,99 +169,71 @@ client.on('message', async msg => {
         }
         if (CardRequest == "Shock-box") { CardRequest = "Shock-Box"; }
         //replacing apostraphies and spaces to make it url friendly
-        var CardToFetch = CardRequest.replace(/ /g,"_");
-        CardToFetch = CardToFetch.replace(/'/g, '%27');
-        CardToFetch = CardToFetch.replace(/:/g, '');
+        var CardToFetch = CardRequest.replace(/ /g, "_").replace(/'/g, '%27').replace(/:/g, '');
         if (CardToFetch == "Boosted_Robo-kick") { CardToFetch = "Boosted_Robo-Kick"; }
         if (CardToFetch == "Enhanced_Robo-kick") { CardToFetch = "Enhanced_Robo-Kick"; }
         var PageToOpen = 'https://griftlands.gamepedia.com/' + CardToFetch
         var ImageToOpen = 'https://griftlands.gamepedia.com/File:' + CardToFetch + ".png"
         console.log("FETCHING " + PageToOpen)
-        
-        var DOMcheck, LoadContent, PageStatus
-        
-        function pStatus(url) {
-            return new Promise(function(resolve, reject) {
+
+        var DOMcheck;
+
+        function fetchPage(url) {
+            return new Promise(function (resolve, reject) {
                 //GRABBING STATUS
-                https.get(url, function(response) {
-                    DOMcheck = response.statusCode
-                    PageToOpen = response.responseUrl
-                    resolve(DOMcheck)
-                }).on('error', function(e) {
+                https.get(url, function (response) {
+                    DOMcheck = response.statusCode;
+                    if (DOMcheck != 404) {
+                        var data = "";
+                        response.on('data', function (chunk) {
+                            data += chunk;
+                        });
+                        response.on('end', function () {
+                            resolve(data);
+                        });
+                    } else {
+                        resolve(DOMcheck);
+                    }
+                }).on('error', function (e) {
                     ErrorMessage(e, SentMessage, GlobalMessage, FetchingCard);
                     reject(e);
                 });
             })
         }
-        
-        function pImageStatus(url) {
-            return new Promise(function(resolve, reject) {
-                https.get(url, function(response) {
-                    DOMcheck = response.statusCode
-                    PageToOpen = response.responseUrl
-                    resolve(DOMcheck)
-                }).on('error', function(e) {
-                    ErrorMessage(e, SentMessage, GlobalMessage, FetchingCard);
-                    reject(e);
-                });
-            });
-        }
-        
-        function pContent(StatusResult) {
-            return new Promise(function(resolve, reject) {
-                if (StatusResult === 404) { //page will not load if 404
-                        resolve(StatusResult);
-                    } else {
-                        https.get(PageToOpen, function(res) {
-                            var data = "";
-                            res.on('data', function (chunk) {
-                                data += chunk;
-                            });
-                            res.on("end", function() {
-                                resolve(data);
-                            });
-                        }).on('error', function(e) {
-                            ErrorMessage(e, SentMessage);
-                            reject(e);
-                        });
-                    }
-            });
-        }
-        
+
         var Title = "N/A"
         var Description = "No Description"
         var HasDescription = false;
         var CardImage = "https://i.ibb.co/LR1xVSM/Icon-Missing.png";
-        var IsUpgradeable = false;
-        
+
         var Categories = []
         var MustInclude = "All cards"
         var AllCardsThere = false;
         var ImageMustInclude = "Card images"
         var CardImagesThere = false;
-        
+
         var TableElements = []
         var SetNode, RarityNode, DeckTypeNode, CardTypeNode, ExpNeededNode, UpgradeableNode;
-        
+
         function FinalEdit(FeStatus, FeContent) {
-            return new Promise(function(resolve, reject) {
+            return new Promise(function (resolve, reject) {
                 if (FeStatus === 404) {
                     NotFound(SentMessage, OriginalRequest);
                     resolve(SentMessage.content);
                 } else {
                     //fetching data with cheerio
                     const $ = cheerio.load(FeContent)
-                    $('#mw-normal-catlinks ul li').each(function() {
+                    $('#mw-normal-catlinks ul li').each(function () {
                         Categories.push($(this).text());
                     });
-                    Categories.forEach(function(item) {
+                    Categories.forEach(function (item) {
                         if (item == MustInclude) {
                             AllCardsThere = true
                         } else if (item == ImageMustInclude) {
                             CardImagesThere = true
                         }
                     });
-                                       
+
                     if (CardImagesThere == true) {//proceeds to next function if it's not a card image
                         Title = $('#firstHeading').text().replace("File:", '');
                         Title = Title.replace(".png", '');
@@ -283,7 +254,7 @@ client.on('message', async msg => {
                                 $("#mw-content-text .mw-parser-output p:first-of-type")
                                     .nextUntil("div")
                                     .addBack()
-                                    .each(function() {
+                                    .each(function () {
                                         if ($(this).attr("class") == "infoboxtable") { //html is out of order on some pages
                                             return
                                         }
@@ -296,7 +267,7 @@ client.on('message', async msg => {
                                 $("#mw-content-text .mw-parser-output p:first-of-type")
                                     .nextUntil("h2")
                                     .addBack()
-                                    .each(function() {
+                                    .each(function () {
                                         if ($(this).attr("class") == "infoboxtable") { //html is out of order on some pages
                                             return
                                         }
@@ -306,11 +277,7 @@ client.on('message', async msg => {
                                         } else return;
                                     });
                             }
-                            //there's probably a better way of doing this but I'm scatterbrained and don't know how
-                            fullHTML = fullHTML.replace(/<i>/g, '*');
-                            fullHTML = fullHTML.replace(/<\/i>/g, '*');
-                            fullHTML = fullHTML.replace(/<b>/g, '**');
-                            fullHTML = fullHTML.replace(/<\/b>/g, '**');
+                            fullHTML = fullHTML.replace(/<i>/g, '*').replace(/<\/i>/g, '*').replace(/<b>/g, '**').replace(/<\/b>/g, '**');
                             if (fullHTML.indexOf("<") > -1 && fullHTML.indexOf(">") > -1) {
                                 Description = $(fullHTML).text();
                             } else {
@@ -318,13 +285,13 @@ client.on('message', async msg => {
                             };
                             HasDescription = true;
                         }
-                        
+
                         if (HasDescription == false) {
                             Description += "\n\n"
                         }
-                        
+
                         //Table items for upgrades, deck and card types.
-                        $(".infoboxtable td").each(function(){
+                        $(".infoboxtable td").each(function () {
                             //I don't know why there are line breaks but they need to be removed
                             var ToPush = $(this).text().replace(/\r?\n|\r/, '');
                             TableElements.push(ToPush);
@@ -342,7 +309,7 @@ client.on('message', async msg => {
                         ExpNeededNode = ++ExpNeeded;
                         var Upgradeable = TableElements.indexOf('Upgradeable');
                         UpgradeableNode = ++Upgradeable;
-                        
+
                         if (Set > 0) {
                             Description += "**Rarity**: " + TableElements[SetNode] + "\n";
                         };
@@ -355,29 +322,29 @@ client.on('message', async msg => {
                         if (CardType > 0) {
                             Description += "**Card Type**: " + TableElements[CardTypeNode];
                         };
-                        
+
                         //Upgrades
-                        if (TableElements[UpgradeableNode] != "no" && TableElements[UpgradeableNode] != "No" && 
-                        $(".infoboxtable tr:contains('Upgrades')").next().length) {
+                        if (TableElements[UpgradeableNode] != "no" && TableElements[UpgradeableNode] != "No" &&
+                            $(".infoboxtable tr:contains('Upgrades')").next().length) {
                             var UpgradeNumber = 1
                             Description += "\n**Exp. Needed**: " + TableElements[ExpNeededNode]
-                            $(".infoboxtable td:contains('Upgrade')").each(function() {
+                            $(".infoboxtable td:contains('Upgrade')").each(function () {
                                 if ($(this).text() != "Upgradeable" && $(this).text() != "upgradeable") {
                                     Description += "\n**Upgrade " + UpgradeNumber + "**: " + $(this).next().text().replace(/\r?\n|\r/, '');
                                     UpgradeNumber++
                                 } else return;
                             });
                         };
-                        
+
                         //Image! Not full-size since wiki has it set up in a weird way but the embed was getting big anyway
                         if ($(".infoboxtable .image").length) {
                             CardImage = $(".infoboxtable .image img").attr('src');
                         } else if ($(".mw-parser-output .thumbimage").length) {
                             CardImage = $(".mw-parser-output .thumbimage").attr('src');
                         }
-                        
+
                         FinalEmbedMessage(SentMessage, Title, Description, CardImage, PageToOpen)
-                        
+
                         resolve(SentMessage.content);
                     } else {
                         NotCardEmbedMessage(SentMessage, CardRequest)
@@ -386,38 +353,21 @@ client.on('message', async msg => {
                 }
             });
         }
-        
-        function CarryOver(ResultToCarry) {
-            return new Promise(function(resolve, reject) {
-                resolve(ResultToCarry);
-            });
-        }
-        
+
         var FetchingImage = false;
         //Dangerous territory, chaining functions, I HAVE NEVER DONE THIS BEFORE AND I DON'T LIKE IT :(
-        pStatus(PageToOpen).then(function(result) {
-            console.log("FIRST RESULT: " + result);
+        fetchPage(PageToOpen).then(function (result) {
             if (result === 404) {
                 console.log("PAGE NOT FOUND, LOOKING FOR IMAGE INSTEAD")
                 FetchingImage = true;
-                return pImageStatus(ImageToOpen);
+                return fetchPage(ImageToOpen);
             } else {
-                console.log("PAGE FOUND: " + PageToOpen);
-                return pContent(result);
+                return FinalEdit(DOMcheck, result);
             }
-        }).then(function(result) {
-            console.log("SECOND RESULT RECEIVED");
-            if (result === 404) {
-                console.log("IMAGE NOT FOUND, CARRYING OVER")
-                return CarryOver(result)
-            } else {
-                return pContent(result);
+        }).then(function (result) {
+            if (FetchingImage) {
+                return FinalEdit(DOMcheck, result);
             }
-        }).then(function(result) {
-            console.log("THIRD RESULT RECEIVED");
-            return FinalEdit(DOMcheck, result);
-        }).then(function(result) {
-            console.log("FINAL MESSAGE SENT");
         }).catch((error) => {
             ErrorMessage(error, SentMessage, GlobalMessage, FetchingCard);
         });
@@ -426,7 +376,7 @@ client.on('message', async msg => {
     } else return;
 });
 
-process.on('unhandledRejection', function(reason, p){
+process.on('unhandledRejection', function (reason, p) {
     console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
     ErrorMessage(reason, GlobalSentMessage, GlobalMessage, FetchingCard);
 });
